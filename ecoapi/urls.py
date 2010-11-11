a4d769c9c4aad1826acba64ff4d105a8f886a80d
Eric St-Jean <esj@wwd.ca>
2010-11-11 16:26:55 -0500

    allow digits in type and method names

--- ecoapi/urls.py
+++ ecoapi/urls.py
@@ -6,10 +6,11 @@ from ecoapi.views import *
 urlpatterns = patterns('ecoapi.views',
         (r'^/?$', TopLevelInterfaceView.as_view()),
         (r'^(?P<ver>[.0-9]+)/?$', InterfaceView.as_view()),
-        (r'^(?P<ver>[.0-9]+)/(?P<type>[a-zA-Z_]+)/(?P<id_or_method>[a-zA-Z_]+)/?$', 
+        (r'^(?P<ver>[.0-9]+)/(?P<type>[a-zA-Z0-9_]+)'
+         r'/(?P<id_or_method>[a-zA-Z0-9_]+)/?$', 
          ObjectOrClassMethodView.as_view()),
-        (r'^(?P<ver>[.0-9]+)/(?P<type>[a-zA-Z_]+)/'
-         r'(?P<id>[a-zA-Z0-9_]+)/(?P<method>[a-zA-Z_]+)/?$', 
+        (r'^(?P<ver>[.0-9]+)/(?P<type>[a-zA-Z0-9_]+)/'
+         r'(?P<id>[a-zA-Z0-9_]+)/(?P<method>[a-zA-Z0-9_]+)/?$', 
          InstanceMethodView.as_view()),
 )
 
