369ace3c1ea45719dcefb65631a25542a3df6c5f
Eric St-Jean <esj@wwd.ca>
2010-11-10 23:33:48 -0500

    basic unittests for json encoding/decoding

---
+++ ecoapi/tests/__init__.py
@@ -0,0 +1,8 @@
+# run with
+# DJANGO_SETTINGS_MODULE=ecoapi.settings python tests/__init__.py
+
+from test_parsers import *
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ ecoapi/tests/test_parsers.py
@@ -0,0 +1,42 @@
+import unittest
+
+from ecoapi.parsers import Decoder, Encoder
+
+class TestJSONDecode(unittest.TestCase):
+
+    def setUp(self):
+        self.decoder = Decoder('json')
+
+    def test_str(self):
+        self.assertEqual(self.decoder.decode('"FOO"'), "FOO")
+        self.assertRaises(TypeError, self.decoder.decode, ('FOO',))
+
+    def test_list(self):
+        self.assertEqual(self.decoder.decode('["FOO"]'), ["FOO"])
+        
+    def test_dict(self):
+        self.assertEqual(self.decoder.decode('{"FOO":"BAR"}'), {"FOO": "BAR"})
+
+    def test_nested(self):
+        self.assertEqual(self.decoder.decode('{"FOO":["BAR"]}'), {"FOO": ["BAR"]})
+
+class TestJSONEncode(unittest.TestCase):
+
+    def setUp(self):
+        self.encoder = Encoder('json')
+
+    def test_str(self):
+        self.assertEqual(self.encoder.encode("FOO"), '"FOO"')
+
+    def test_list(self):
+        self.assertEqual(self.encoder.encode(["FOO",]), '["FOO"]')
+        
+    def test_dict(self):
+        self.assertEqual(self.encoder.encode({"FOO": "BAR"}), '{"FOO": "BAR"}')
+
+    def test_nested(self):
+        self.assertEqual(self.encoder.encode({"FOO":["BAR"]}), '{"FOO": ["BAR"]}')
+
+if __name__ == '__main__':
+    unittest.main()
+
