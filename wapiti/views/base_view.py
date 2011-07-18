185832400be8b8ef188d99ab554bf15ff071b7bb
Eric St-Jean <esj@wwd.ca>
2011-07-18 13:43:58 -0400

    typo, fixes traceback from this morning

--- wapiti/views/base_view.py
+++ wapiti/views/base_view.py
@@ -215,7 +215,7 @@ class WapitiBaseView(View):
         else:
             authorized = apikey.is_authorized(request)
 
-        lim = apikey.check_limits(request)
+        lim = api_key.check_limits(request)
         if lim != True:
             return APIRateLimit("Limit exceeded: %s"%lim)
 
